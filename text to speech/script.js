1cffa872e25f226e55881489e8b47442f1f511d5
PravinPatil07 <162779622+PravinPatil07@users.noreply.github.com>
2024-10-06 17:37:45 +0530

    Add files via upload

---
+++ text to speech/script.js
@@ -0,0 +1,22 @@
+let speech = new SpeechSynthesisUtterance();
+
+let addVoices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () =>{
+    addVoices = window.speechSynthesis.getVoices();
+    speech.voice = addVoices[0];
+
+    addVoices.forEach((voice,i)=>(voiceSelect.options[i] = new Option(voice.name,i)))
+}
+
+voiceSelect.addEventListener("change",()=>{
+    speech.voice = addVoices[voiceSelect.value]
+})
+
+let button = document.querySelector("button")
+button.addEventListener("click",()=>{
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech)
+});
